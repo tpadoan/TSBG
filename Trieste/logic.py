331fd1ebc73efc3d8587e2a4eca9ed6be1fd7417
Tommaso Padoan <tommaso.padoan@units.it>
2023-09-11 16:25:32 +0200

    Update logic.py

--- Trieste/logic.py
+++ Trieste/logic.py
@@ -1,6 +1,9 @@
+# maximum number of turns to catch mr.X
 maxTurns = 10
-reveals = [3,6,10]
+# list of the turns when mr.X location is revealed to everyone, possibly empty
+reveals = []
 
+# this, and the following files, must point to the new adjacency matrix, taxi, bus and ug arcs, correspondingly
 f = open("data/matrix.txt", "r", encoding="utf8")
 content = f.read()
 f.close()
@@ -36,6 +39,7 @@ for s in content.split('\n'):
 
 E = {(i+1):((taxi[i+1].union(bus[i+1])).union(ug[i+1])) for i in range(size)}
 
+# the strings/moves in this function must be properly renamed
 def moves(place):
   m = ['taxi']
   if bus[place]:
@@ -44,6 +48,7 @@ def moves(place):
     m.append('ug')
   return m
 
+# the strings/moves in this function must be properly renamed
 def restrictedMoves(state):
   m = []
   if taxi[state[2]].difference(state[1]):
@@ -54,6 +59,7 @@ def restrictedMoves(state):
     m.append('ug')
   return m
 
+# the strings/moves in this function must be properly renamed
 def dest(place, move):
   if move == 'taxi':
     return taxi[place]
