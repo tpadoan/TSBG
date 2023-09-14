dc131f3e5f35b19d633ad200ed79b563f3e38082
dbasso98 <dav.basso98@gmail.com>
2023-09-14 17:31:36 +0200

    Additional graph utils functions

--- utils/graph_util.py
+++ utils/graph_util.py
@@ -29,3 +29,35 @@ def generate_graph() -> nx.Graph:
         G.add_edge(*edge, type='tram')
 
     return G
+
+def connections(graph: nx.Graph, node: int) -> np.ndarray[int]:
+    """ Find the valid connections from one point ot another one, specifying also which 
+        transport can be used to reach the destination node from the source.
+
+    Args:
+        graph (nx.Graph): The nodes' game graph.
+        node  (int): The source node.
+
+    Returns:
+        _np.ndarray[int]: Encoding of all the connections from the source node.
+    """
+    connections = graph.edges(node, data=True)
+    encoded_connections = []
+    for source, dest, data in connections:
+        transport_type = data["type"]
+        encoded_connections.append([source, dest]+[1 if transport_type == t else 0 for t in ['boat', 'tram', 'cart']])
+
+    return np.array(encoded_connections)
+
+
+def node_one_hot_encoding(node_id: int, num_nodes: int) -> list[int]:
+    """ Generate a one-hot encoding of the input node in the graph.
+
+    Args:
+        node_id (int): Input node.
+        num_nodes (int): Total number of nodes in the graph.
+
+    Returns:
+        list[int]: The one-hot encoding.
+    """
+    return [1 if i == node_id - 1 else 0 for i in range(num_nodes)]
