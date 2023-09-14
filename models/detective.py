037c0e369b7babfb68cd89a44b221d0e565a5b13
dbasso98 <dav.basso98@gmail.com>
2023-09-14 08:50:38 +0200

    Models for detective and mrX

---
+++ models/detective.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class Model(nn.Module):
+
+    def __init__(self):
+        super(Model, self).__init__()
+        self.learning_rate = 1e-3
+        self.columns = 1432
+
+        self.hidden1 = int(self.columns / 2)
+        self.hidden2 = int(self.columns / 2)
+        self.hidden3 = int(self.columns / 2)
+        # self.hidden4 = int(self.columns / 2)
+        # self.hidden5 = int(self.columns / 4)
+        # self.hidden6 = int(self.columns / 4)
+        # self.hidden7 = int(self.columns / 4)
+        # self.hidden8 = int(self.columns / 4)
+        # self.hidden9 = int(self.columns / 8)
+        # self.hidden10 = int(self.columns / 8)
+        # self.hidden11 = int(self.columns / 8)
+        # self.hidden12 = int(self.columns / 8)
+        # self.hidden13 = int(self.columns / 8)
+        # self.hidden14 = int(self.columns / 16)
+        # self.hidden15 = int(self.columns / 16)
+        # self.hidden16 = int(self.columns / 16)
+        # self.hidden17 = int(self.columns / 16)
+        # self.hidden18 = int(self.columns / 16)
+        # self.hidden19 = int(self.columns / 32)
+        # self.hidden20 = int(self.columns / 32)
+        self.out = 1
+
+        self.fc1 = nn.Linear(self.columns, self.hidden1)
+        self.fc2 = nn.Linear(self.hidden1, self.hidden2)
+        self.fc3 = nn.Linear(self.hidden2, self.hidden3)
+        self.fc_out = nn.Linear(self.hidden3, self.out)
+
+        self.relu = nn.ReLU()
+        
+        self.loss_function = nn.CrossEntropyLoss()
+        self.optimizer = optim.Adam(self.parameters(), lr=self.learning_rate)
+        
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.relu(self.fc3(x))
+        x = self.fc_out(x)
+        return x
+
+    def optimize(self, x, y):
+        x = torch.tensor(x, dtype=torch.float32)
+        y = torch.tensor(y, dtype=torch.float32)
+        predictions = self(x)
+        loss = self.loss_function(predictions, y)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+    def save(self, episode):
+        # Saving the model state dictionary to a file
+        torch.save(self.state_dict(), f'models/detective_models/model_{episode}.pth')
+
+    def restore(self, episode):
+        # Loading a model state dictionary from a file
+        self.load_state_dict(torch.load(f'models/detective_models/model_{episode}.pth'))
