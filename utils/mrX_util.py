505ddddfc0a5b4188ccbe21e5e46c28e5a562d00
dbasso98 <dav.basso98@gmail.com>
2023-09-27 18:50:37 +0200

    Finalized stable baselines 3 model MaskedPPO with masked actions based on detective_id Adjusted game.py to be compatible with the new model

--- utils/mrX_util.py
+++ utils/mrX_util.py
@@ -3,7 +3,7 @@ import numpy as np
 
 import utils.graph_util
 
-def valid_moves_list(graph: nx.Graph, mrX: np.ndarray[int]) -> np.ndarray[int]:
+def valid_moves_list(graph: nx.Graph, mrX: np.ndarray[int], detectives: np.ndarray[int]) -> np.ndarray[int]:
     """ Get the list of valid moves that can be done by mrX.
 
     Args:
@@ -17,5 +17,7 @@ def valid_moves_list(graph: nx.Graph, mrX: np.ndarray[int]) -> np.ndarray[int]:
     current_node = mrX[0]
     # Get the encoding of all the edges that can be reached from the current node
     edges = utils.graph_util.connections(graph, current_node)
+    detective_nodes = [node[0] for node in detectives]
+    valid_edges = [edge for edge in edges if edge[1] not in detective_nodes]
 
-    return np.array(edges)
+    return np.array(valid_edges)
