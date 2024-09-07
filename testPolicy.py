f21ff2065a34eff4f1825123ff26f900c7b59454
Tommaso Padoan <tommaso.padoan@units.it>
2024-09-07 10:15:52 +0200

    updated policies tests

--- testPolicy.py
+++ testPolicy.py
@@ -10,8 +10,6 @@ maxTurns = 10
 numDetectives = 3
 # flag for fixed initial positions of players, only working if numDetectives < 4
 fixed = False
-# number of tests
-numTests = 1000
 
 boat = {(i+1):[] for i in range(sizeGraph)}
 f = open("data/boat.txt", "r", encoding="utf8")
@@ -69,46 +67,88 @@ def min_shortest_path(state, node):
     dist += 1
   return dist
 
-def mrXmove1(state):
+def mrXmove(state, strat):
   best = 0
-  maxDist = -1
-  actions = dest(state[1])
-  for act in actions:
-    dist = min_shortest_path(state, act)
-    if maxDist < dist:
-      best = act
-      maxDist = dist
+  if strat == 0:
+    maxDist = -1
+    actions = dest(state[1])
+    for act in actions:
+      dist = min_shortest_path(state, act)
+      if maxDist < dist:
+        best = act
+        maxDist = dist
+  elif strat == 1:
+    maxSize = -1
+    actions = dest(state[1])
+    for act in actions:
+      size = len(propagate(state, transportFor(state[1], act)))
+      if maxSize < size:
+        best = act
+        maxSize = size
+  elif strat == 2:
+    maxDistSize = -1
+    actions = dest(state[1])
+    for act in actions:
+      dist = min_shortest_path(state, act)
+      size = len(propagate(state, transportFor(state[1], act)))
+      if maxDistSize < dist*size:
+        best = act
+        maxDistSize = size
+  elif strat == 3:
+    maxDistSize = -1
+    actions = cart[state[1]]
+    for act in actions:
+      dist = min_shortest_path(state, act)
+      size = len(propagate(state, transportFor(state[1], act)))
+      if maxDistSize < dist*size:
+        best = act
+        maxDistSize = dist*size
+    if maxDistSize < 2:
+      actions = tram[state[1]]
+      for act in actions:
+        dist = min_shortest_path(state, act)
+        size = len(propagate(state, transportFor(state[1], act)))
+        if maxDistSize < dist*size:
+          best = act
+          maxDistSize = dist*size
+    if maxDistSize < 2:
+      actions = boat[state[1]]
+      for act in actions:
+        dist = min_shortest_path(state, act)
+        size = len(propagate(state, transportFor(state[1], act)))
+        if maxDistSize < dist*size:
+          best = act
+          maxDistSize = dist*size
+  elif strat == 4:
+    actionsCart = cart[state[1]]
+    actionsTram = tram[state[1]]
+    actionsBoat = boat[state[1]]
+    moves = actionsCart + actionsTram + actionsBoat
+    prob = []
+    for act in actionsCart:
+      dist = min_shortest_path(state, act)
+      prob.append(dist)
+    for act in actionsTram:
+      dist = min_shortest_path(state, act)
+      prob.append(dist / 2.0)
+    for act in actionsBoat:
+      dist = min_shortest_path(state, act)
+      prob.append(dist / 3.0)
+    tot = sum(prob)
+    if tot > 0:
+      for i in range(len(prob)):
+        prob[i] = prob[i] / tot
+      best = choice(moves, p=prob)
+    else:
+      best = moves[0]
   return best
 
-def mrXmove2(state):
-  best = 0
-  maxSize = -1
-  actions = dest(state[1])
-  for act in actions:
-    size = len(propagate(state, transportFor(state[1], act)))
-    if maxSize < size:
-      best = act
-      maxSize = size
-  return best
-
-def mrXmove3(state):
-  best = 0
-  maxDistSize = -1
-  actions = dest(state[1])
-  for act in actions:
-    dist = min_shortest_path(state, act)
-    size = len(propagate(state, transportFor(state[1], act)))
-    if maxDistSize < dist*size:
-      best = act
-      maxSize = dist*size
-  return best
-
-def run(G):
+def run(G, mrXstrat, useRL):
   mrX = None
   police = None
   if fixed and numDetectives<4:
-    mrX = 5
-    police = [20-7*i for i in range(numDetectives)]
+    mrX = 13
+    police = [5,6,20]
   else:
     police = choice(list(range(1, sizeGraph+1)), size=numDetectives, replace=False)
     mrX = 0
@@ -119,35 +159,40 @@ def run(G):
       if maxDist < dist:
         mrX = pos
         maxDist = dist
-  G.initGame(police, mrX)
+  G.initGame(police, mrX, useRL)
   state = [police, mrX, {mrX}]
   turn = 0
-  stop = False
   found = False
-  while turn < maxTurns and not found and not stop:
+  while turn < maxTurns and not found:
+    if list(state[0]) != list(G.state[0]):
+      print('Error: inconsistent detective state')
+    if state[2] != set(G.state[1].keys()):
+      print('Error: inconsistent mrX inferred locations')
+    if state[1] not in state[2]:
+      print('Error: inconsistent mrX position w.r.t. inferred ones')
     turn += 1
-    mrX = mrXmove1(state)
+    mrX = mrXmove(state, mrXstrat)
     move = transportFor(state[1], mrX)
     state[1] = mrX
     state[2] = propagate(state, move)
     if state[1] in state[0]:
       found = True
       break
-    state[0] = G.playTurn(move)
+    state[0], _ = G.playTurn(move)
     state[2] = state[2].difference(state[0])
-    if not len(state[0]):
-      stop = True
     if state[1] in state[0]:
       found = True
   return found
 
-
 if __name__ == '__main__':
   G = Game()
-  wins = 0
-  print(f"Testing {numTests} runs")
-  print("Run\tD_wins\tX_wins")
-  for count in range(numTests):
-    if run(G):
-      wins += 1
-    print(f"{1+count}\t{wins}\t{1+count-wins}")
+  numTests = 10000
+  useRL = True
+  for mrXstrat in range(5):
+    wins = 0
+    print(f"\nTesting {numTests} runs with {'RL' if useRL else 'probabilistic'} detectives, mrX strat {mrXstrat}")
+    print("Run\tD_wins\tX_wins\tD_win%o")
+    for count in range(numTests):
+      if run(G, mrXstrat, useRL):
+        wins += 1
+    print(f"{numTests}\t{wins}\t{numTests-wins}\t{int(1000*wins/numTests)}")
