633f76299836e02043c206a7a1cb6724e86435e8
Emanuele Ballarin <emanuele@ballarin.cc>
2024-09-18 16:29:26 +0200

    Update

---
+++ prepdl.ps1
@@ -0,0 +1,8 @@
+#!/usr/bin/env pwsh
+python -m venv .python_env_win
+.python_env_win\Scripts\activate.ps1
+pip install pip -U
+pip install uv -U
+uv pip install Pillow numpy stable_baselines3 sb3_contrib
+cd models
+./getmodels.ps1
